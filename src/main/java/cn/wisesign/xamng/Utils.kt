03da46fdd53d0fbbe7c4cc04f911b9d3893e78be
github1978 <umbrella1978@live.cn>
2016-12-27 23:56:08 +0800

    增加velocity以及angular - mobile UI

--- src/main/java/cn/wisesign/xamng/Utils.kt
+++ src/main/java/cn/wisesign/xamng/Utils.kt
@@ -1,8 +1,28 @@
 package cn.wisesign.xamng
 
-import java.net.URL
+import org.apache.commons.collections.ExtendedProperties
+import org.apache.velocity.runtime.resource.Resource
+import org.apache.velocity.runtime.resource.loader.ResourceLoader
+import java.io.InputStream
 
 
-fun httpGet(url:String,param:String=""):String{
-    return URL(url+"?"+param).readText()
+class VelocityLoader : ResourceLoader(){
+
+    override fun init(p0: ExtendedProperties?) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getResourceStream(source: String?): InputStream {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return VelocityLoader::class.java.getResourceAsStream(source)
+    }
+
+    override fun getLastModified(p0: Resource?): Long {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isSourceModified(p0: Resource?): Boolean {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
