b6f2df648c0941552fe1d9cd39aafe8333503277
zhuxiaofeng <zhuxiaofeng@wisesignsoft.com>
2017-03-15 18:30:21 +0800

    错误处理更新内容

--- src/main/java/cn/wisesign/xamng/module/BreakToolModule.kt
+++ src/main/java/cn/wisesign/xamng/module/BreakToolModule.kt
@@ -1,6 +1,6 @@
 package cn.wisesign.xamng.module
 
-import cn.wisesign.xamng.getToDayDate
+import cn.wisesign.xamng.*
 import org.nutz.dao.Cnd
 import org.nutz.dao.Sqls
 import org.nutz.dao.sql.Sql
@@ -19,6 +19,9 @@ class BreakToolModule :BaseModule() {
     companion object{
         var STRIKE_SIGN:String = "FIXED"
         var DM_RECEIVABLES:String = "dm_receivables"
+        var DM_CHARGE_RECORD:String = "dm_charge_record"
+        var DM_CHARGE_Bills:String = "dm_charge_bills"
+        var COMMON_DATE_FORMAT:String = "yyyy-MM-dd HH:mm:ss"
     }
 
     @At
@@ -39,10 +42,22 @@ class BreakToolModule :BaseModule() {
         if(listStrikeReceivables.isNotEmpty()){
             generateStrikeReceivables(listStrikeReceivables)
         }
-
         // 2. 处理因迁出导致错帐的收费记录
-        // 2.1
+        // 2.1 查询迁出日期所在的收费记录
+        var wrongCharge = queryUnStrikeChargeRecord(breakDate,customerName,platformName,platformVersion,moduleName)
+        // 2.2 修改查询出的收费记录
+        // 2.2.1 计算冲正后的金额
+        var strikeChargeStartDate = wrongCharge["THIS_CHARGE_STARTTIME"].toString()
+        var strikeChargeDays = getDateSpace(strikeChargeStartDate, breakDate+" 00:00:00").toInt()
+        if(getMonthDay(breakDate).equals("0229") || getMonthDay(strikeChargeStartDate).equals("0229")){
+            strikeChargeDays = strikeChargeDays - 1
+        }
+        var yearMoney = getYearMoneyByRelatedChargeBills(wrongCharge["REL_CHARGE"].toString(),strikeChargeStartDate)
+        var strikeChargeMoney = (yearMoney.toFloat()/365)*strikeChargeDays
+        println(strikeChargeMoney)
+        // 2.2.2 计算差额冲正
 
+        // 2.2.3
 
         // 删除错帐收费记录
 
@@ -50,6 +65,25 @@ class BreakToolModule :BaseModule() {
         // 删除错帐收费单
 
 
+    }
+
+    // 查询迁出日期所在的收费记录
+    private fun queryUnStrikeChargeRecord(
+            breakDate:String,
+            customerName:String,
+            platformName:String,
+            platformVersion:String,
+            moduleName:String
+    ):Map<*,*>{
+        var sql:Sql = Sqls.create(
+                "select * from $DM_CHARGE_RECORD where THIS_CHARGE_STARTTIME<='$breakDate' and "+
+                        "THIS_CHARGE_ENDTIME>='$breakDate' and PLATFORM='$platformName' and MODULE='$moduleName'"+
+                        " and VERSION='$platformVersion' and CLIENT_NAME='$customerName'"
+        )
+        sql.setCallback(Sqls.callback.maps())
+        dao.execute(sql)
+        var listWrongCharge = sql.getList(Map::class.java)
+        return listWrongCharge[0]
     }
 
     // 查询符合条件的待冲应收单
@@ -124,5 +158,23 @@ class BreakToolModule :BaseModule() {
     // 通用的生成应收单方法
     private fun generateReceivables() {
 
+    }
+
+    private fun getYearMoneyByRelatedChargeBills(
+            chargeBillsObjectId:String,chargeRecordStartDate:String):String{
+        var sql:Sql = Sqls.create("select * from $DM_CHARGE_Bills where objectid='$chargeBillsObjectId'")
+        sql.setCallback(Sqls.callback.maps())
+        var strikeChargeBill = dao.execute(sql).getList(Map::class.java)[0]
+        var strikeChargeBillStartDate = strikeChargeBill["TOLL_STARTTIME"].toString()
+
+        var firstYearEndStamp = getDateByYearStamp(strikeChargeBillStartDate,1,COMMON_DATE_FORMAT)
+        var secondYearEndStamp = getDateByYearStamp(strikeChargeBillStartDate,2,COMMON_DATE_FORMAT)
+        var chargeRecordStartDateStamp = date2TimeStamp(chargeRecordStartDate,COMMON_DATE_FORMAT)
+
+        return when{
+            chargeRecordStartDateStamp<=firstYearEndStamp -> strikeChargeBill["FIRST_REAL_MONEY"].toString()
+            chargeRecordStartDateStamp<=secondYearEndStamp -> strikeChargeBill["SECOND_REAL_MONEY"].toString()
+            else -> strikeChargeBill["THIRD_REAL_MONEY"].toString()
+        }
     }
 }
